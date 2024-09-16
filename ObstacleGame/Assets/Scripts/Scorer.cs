a1411eff9d827c2d10da988bfd7af2037fef4196
mcdani978 <mcdani978@morrisville.edu>
2024-09-16 15:36:51 -0400

    Using Tags

--- ObstacleGame/Assets/Scripts/Scorer.cs
+++ ObstacleGame/Assets/Scripts/Scorer.cs
@@ -8,7 +8,11 @@ public class Scorer : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        hits++;
-        Debug.Log("You've bumped into a thing this many times:" + hits);
+        if (other.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("You've bumped into a thing this many times:" + hits);
+        }
     }
 }
+
